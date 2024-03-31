d8a1da1e04d9bdd2c6157bb661d93297835cbd79
Techno-Logy <164623811+Techno-Logy@users.noreply.github.com>
2024-03-31 18:51:32 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+
+    default = "us-east-1"
+}
+
+variable "os-name" {
+    default = "ami-0c101f26f147fa7fd"
+}
+
+variable "key" {
+    default = "AWS_KEY"
+}
+
+variable "instance-type" {
+    default = "t2.micro"
+}
+
+variable "vpc-cidr" {
+    default = "10.0.0.0/16"
+}
+
+variable "subnet_az" {
+    default = "us-east-1a"
+}
